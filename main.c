93f901ea6b81d67a9c613b6752f60aa97ca61c13
root <sushilpande73@gmail.com>
2019-11-01 17:13:53 +0530

    applied security changes to feature2

--- main.c
+++ main.c
@@ -5,18 +5,18 @@ Initial Function()
   feature2()
 }
 
-<<<<<<< HEAD
+
 feature2()
 {
   //devloper Code for Feature 2
 }
-=======
+
 Initial Security()
 {
  +security change
 }
 
->>>>>>> master
+
 
 
 }
