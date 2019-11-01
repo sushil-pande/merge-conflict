fb1e24ab4e33e5f15a012da84b939b16422ef124
root <sushilpande73@gmail.com>
2019-11-01 18:02:11 +0530

    resolved merge conflict

--- main.c
+++ main.c
@@ -2,29 +2,23 @@ pseudo code
 {
 Initial Function()
 {
-<<<<<<< HEAD
-  feature1()
+  //Sample Code
 }
 
 feature1()
 {
   //Devloper Code for Feature 1
-=======
-  feature2()
 }
-
 
 feature2()
 {
   //devloper Code for Feature 2
->>>>>>> feature2
 }
 
 Initial Security()
 {
  +security change
 }
-
 
 
 
